6ae583ac7f2e78e17762c30ea71bc823880b0180
Tobi <tobi.agbabi@yahoo.com>
2021-07-22 16:24:28 +0100

    'inventoryapp'

---
+++ Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryApp
+{
+    class Database
+    {
+
+        public SQLiteConnection Myconnection;
+
+
+
+
+        public void CreateConnection()
+        {
+
+            // Create a new database connection:
+            Myconnection = new SQLiteConnection("Data Source=database.db;Version=3;New = True;Compress = True;");
+
+         // Open the connection:
+            try
+            {
+                Myconnection.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"no connection.{ex.Message}");
+            }
+            
+        }
+    }
+}
